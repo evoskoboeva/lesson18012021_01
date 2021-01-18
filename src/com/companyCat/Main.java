cdf491d2f28638419c6c38f3603490429d80806d
evoskoboeva <GITHUB evoskoboeva@tango.com.ua>
2021-01-18 23:33:51 +0200

    Initial commit 1234567

--- src/com/companyCat/Main.java
+++ src/com/companyCat/Main.java
@@ -3,6 +3,11 @@ package com.companyCat;
 public class Main {
 
     public static void main(String[] args) {
+<<<<<<< HEAD
+=======
+        System.out.println("Hello Kat");
+        System.out.println("Hello 123");
+>>>>>>> d6786e8 (Initial commit 1234567)
 	// write your code here
     }
 }
